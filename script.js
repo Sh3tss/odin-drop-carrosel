825509f466061d1819ccac95901713cc66673bb2
Sh3tss <gabrielggartist@gmail.com>
2025-08-14 20:33:20 -0300

    creatd the loop for the carrousel

--- script.js
+++ script.js
@@ -19,3 +19,41 @@ window.addEventListener('click', (event) => {
         })
     }
 })
+
+//carrousel part
+const carrouselContainer = document.querySelector('.carousel-slide');
+const images = document.querySelectorAll('.carousel-slide img');
+const previusBtn = document.querySelector('.prev-button');
+const nextBtn = document.querySelector('.next-button');
+const navigationDot = document.querySelectorAll('.dot')
+let currentSlide = 0;
+
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= images.length){
+        currentSlide = 0;
+    }
+    showSlide(); 
+}
+
+function prevSlide(){
+    currentSlide--;
+    if (currentSlide < 0){
+        currentSlide = images.length - 1;
+    }
+    showSlide(); 
+}
+
+function showSlide(){
+    const moveAmount = -currentSlide * 600;
+    carrouselContainer.style.transform = `translateX(${moveAmount}px)`;
+}
+
+nextBtn.addEventListener('click', (event) => {
+    nextSlide();
+})
+
+previusBtn.addEventListener('click', (event) => {
+    prevSlide();
+})
